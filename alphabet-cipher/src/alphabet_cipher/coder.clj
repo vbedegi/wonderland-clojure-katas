8b1da2a8d9ce924c92bc8f19e7441d354803902d
vbedegi <vbedegi@gmail.com>
2015-01-04 19:27:05 +0100

    alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,27 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet (cycle "abcdefghijklmnopqrstuvwxyz"))
+
+(defn char->int [c]
+  (- (int c) 97))
+
+(defn int->char [i]
+  (char (+ i 97)))
+
+(defn codec [f keyword message]
+  (apply str (map f message (cycle keyword))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [encode-chars (fn [c1 c2]
+                       (let [offset (+ (char->int c1)
+                                       (char->int c2))]
+                         (-> offset (drop alphabet) first)))]
+    (codec encode-chars keyword message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (letfn [(decode-chars [c1 c2]
+                        (->> (drop (char->int c2) alphabet)
+                             (take-while #(not= % c1))
+                             count
+                             int->char))]
+    (codec decode-chars keyword message)))
